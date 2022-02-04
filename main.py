4f5c6d0f3739cd4102f18e16ccb9fb12ef6bfac8
btrosso <51491995+btrosso@users.noreply.github.com>
2022-02-04 14:04:29 -0600

    optimized some logic

--- main.py
+++ main.py
@@ -30,31 +30,27 @@ scissors = '''
 #Write your code below this line 👇
 
 user_choice = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors."))
-
 comp_choice = random.randint(0,2)
 
-key = [[1, rock, "rock"], [2, paper, "paper"], [3, scissors, "scissors"]]
+game_images = [rock, paper, scissors]
+game_choices = ["rock", "paper", "scissors"]
+choice_list = [game_images, game_choices]
 
-print(f"You chose: {key[user_choice][2]}")
-print(f"{key[user_choice][1]}")
-print(f"The computer chose: {key[comp_choice][2]}")
-print(f"{key[comp_choice][1]}")
+print(f"You chose: {choice_list[1][user_choice]}")
+print(f"{choice_list[0][user_choice]}")
+
+print(f"The computer chose: {choice_list[1][comp_choice]}")
+print(f"{choice_list[0][comp_choice]}")
 
 if user_choice == comp_choice:
   print("Its a DRAW.")
-else:
-  if user_choice == 1:
-    if comp_choice == 2:
-      print("You Lose.")
-    else:
-      print("You Win!")
-  elif user_choice == 2:
-    if comp_choice == 3:
-      print("You Lose.")
-    else:
-      print("You Win!")
-  else:
-    if comp_choice == 1:
-      print("You Lose.")
-    else:
-      print("You Win!")
+elif user_choice == 0 and comp_choice == 2:
+  print("YOU WIN!")
+elif user_choice == 2 and comp_choice == 0:
+  print("You Lose.")
+elif user_choice < comp_choice:
+  print("You Lose")
+elif user_choice > comp_choice:
+  print("YOU WIN!")
+elif user_choice > 2:
+  print("Error: Choose either 0, 1, or 2")
